c72f9b43382c82986f787c15d48ed8e074650589
shreeyasthaa <shreeyashrestha428@gmail.com>
2025-06-01 19:40:31 +0545

    added basic structure

---
+++ basicstru.c
@@ -0,0 +1,29 @@
+//basic structure
+#include<stdio.h>
+int main(){
+	int ch;
+	printf("\t\t\t\t**Welcome to mini notepad!**\t\t\t\t");
+	printf("\n\t1.Create a New File");
+	printf("\n\t2.Open Existing File");
+	printf("\n\t3.Save Current File");
+	printf("\n\t4.Exit editor");
+	printf("\n\tEnter your choice: ");
+	scanf("%d",&ch);
+	switch(ch){
+		case 1:
+			
+		break;
+		case 2:
+			
+		break;
+		case 3:
+		
+		break;
+		case 4:
+		
+		break;
+		
+			
+	}
+	return 0;
+}
