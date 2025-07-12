c1322001eb1a58b3c96b8b28c31d81748f05fee8
shreeyasthaa <shreeyashrestha428@gmail.com>
2025-07-12 15:32:10 +0545

    touched up the menu and added delete function

--- basicstru.c
+++ basicstru.c
@@ -6,7 +6,7 @@ void createFile(char filename[100], char extension[20]){
     FILE *p;
     char fullFilename[120];
     sprintf(fullFilename, "%s.%s", filename, extension);
-    p=fopen(fullFilename,"w");
+    p=fopen(fullFilename,"a");
     if(p==NULL){
     	printf("The file couldnt be created!");
 	}
@@ -51,17 +51,45 @@ void newfile(){
 	      createFile(actualFilename, extension);
     }
 }
-
+//void editfile(){
+//	int n;
+//	FILE *p;
+//	printf("\nEnter which file you want to edit: ");
+//	 if (fgets(n, sizeof(n), stdin)){
+//        
+//	 }
+//
+//}
+void deletefile(){
+	char filename[120];
+    printf("\nEnter the file you want to delete: ");
+	if (scanf("%s", filename) != 1) {   /* guard against bad input ani filename array bhayera & chaidaina*/
+	    printf("Invalid input");
+	    exit(0);
+	}
+	if (remove(filename) == 0) {
+	    puts("File deleted successfully.");
+	} 
+	else {
+	    printf("Error deleting file!!");     /* prints the OS error */
+	}    
+}
 	
 int main(){
 	int ch;
+	char choice;
 	printf("\t\t\t\t**Welcome to mini notepad!**\t\t\t\t");
+	initial:
+	printf("\n===========================\n");
+	printf("***Menu***");
 	printf("\n1.Create a New File");
-	printf("\n2.Open Existing File");
-	printf("\n3.Save Current File");
-	printf("\n4.Load existing files");
-	printf("\n5.Exit editor");
-	printf("\nEnter your choice: ");
+	printf("\n2.Open Existing File");//
+	printf("\n3.Load all existing files");
+	printf("\n4.Edit a File");
+	printf("\n5.Delete a file");
+	printf("\n6.Exit editor");
+	printf("\n===========================\n");
+	printf("Enter your choice: ");
 	scanf("%d",&ch);
 	switch(ch){
 		case 1:
@@ -74,10 +102,30 @@ int main(){
 		
 		break;
 		case 4:
-		
+		 // editfile();
 		break;
-		
-			
+		case 5: {  
+		   loadfile();                       
+    	   deletefile();  
+		break;  
+		}                    	
+		case 6:
+			end:
+			printf("\nExiting notepad...");
+			printf("\nExited successfully!");
+			exit(0);
+		default:
+			printf("Error!");
 	}
+	printf("\nDo you want to continue?Y/N ");
+	scanf(" %c",&choice);
+	if(choice=='Y'||choice=='y'){
+		goto initial;
+	}
+	else{
+		printf("Bye bye!");
+		goto end;
+	}
+	
 	return 0;
 }
